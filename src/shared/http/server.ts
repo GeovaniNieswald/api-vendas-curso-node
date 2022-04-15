40f59e2eeb1eb709f3a2eef1f65121a9e2ad3caf
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2022-04-15 14:52:11 -0300

    Adiciona middleware de controle de requisições
    - Limita o número de requisições por segundo que um mesmo IP pode disparar (5 por segundo)

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,13 +9,20 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import { pagination } from 'typeorm-pagination';
 import routes from './routes';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
+app.use(rateLimiter);
+
 app.use(pagination);
+
 app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.use(errors());
